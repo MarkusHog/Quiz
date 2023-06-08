bf7d99b9ee1dd01a44646a62edab0ff0d80fcdd5
MarkusHog <112397244+MarkusHog@users.noreply.github.com>
2023-06-08 12:34:23 +0200

    Created model question

---
+++ Quiz/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿namespace Quiz.Models;
+
+public class Question
+{
+	public Guid Id { get; set; }
+	public string QuizQuestion { get; set; }
+	public string CorrectAnswer { get; set; }
+
+	public string IncorrectAnswer1 { get; set; }
+	public string IncorrectAnswer2 { get; set; }
+	public string IncorrectAnswer3 { get; set; }
+	public string IncorrectAnswer4 { get; set; }
+
+	public bool Correct { get; set; }
+	public int NumberOfCorrectAnswer { get; set; }
+}

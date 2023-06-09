97a475676451a54e5b68367d8dbd78259c1bc9d1
MarkusHog <112397244+MarkusHog@users.noreply.github.com>
2023-06-09 14:08:21 +0200

    updated api url.

--- Quiz/Data/RestDataService.cs
+++ Quiz/Data/RestDataService.cs
@@ -14,7 +14,7 @@ public class RestDataService : IRestDataService
     public RestDataService()
     {
         _httpClient = new HttpClient();
-        _baseAdress = "http://localhost:32776";
+        _baseAdress = "https://markusquiz--bsvbg18.gentlebush-8ad4885e.westeurope.azurecontainerapps.io";
         _url = $"{_baseAdress}/questions";
 
         _jsonSerializerOptions = new JsonSerializerOptions
